05cc0ddfb695e92f4522f06cedd279e33262e6d0
allan <allanmathenge2@gmail.com>
2023-06-19 18:01:59 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname});
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/Hello(.html)?', (req, res, next) => {
+    console.log('Attemted to load Hello.html!');
+    next()
+}, (req, res) => {
+    res.send('Hello world!');
+})
+
+//Chaining route handlers
+const one = (req, res, next) => {
+    console.log('One');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('Two');
+    next()
+}
+
+const three = (req, res) => {
+    console.log('Three');
+    res.send('Finished!')
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
