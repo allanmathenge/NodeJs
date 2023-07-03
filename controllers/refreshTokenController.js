8486d3054c0fb917801a04bbb039d82d43b98015
allan <allanmathenge2@gmail.com>
2023-07-03 15:35:40 +0300

    Cors origin test

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -9,7 +9,6 @@ require('dotenv').config();
 const handleRefreshtoken = (req, res) => {
     const cookies = req.cookies;       
     if (!cookies?.jwt) return res.sendStatus(401); //unauthorized
-    console.log(cookies.jwt)
     const refreshToken = cookies.jwt;
 
     const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
@@ -20,7 +19,7 @@ const handleRefreshtoken = (req, res) => {
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET,
         (err, decoded) => {
-            if(err || foundUser.username !== decoded.username) return res.sendStatus(403);
+            if(err || foundUser.username !== decoded.username) return res.sendStatus(403); //forbidden
             const accessToken = jwt.sign(
                 { "username": decoded.username },
                 process.env.ACCESS_TOKEN_SECRET,
